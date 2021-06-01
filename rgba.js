dbe074b9ba92dc60d98b0f4c3116da5465a83ab4
m-irfan244 <79442665+m-irfan244@users.noreply.github.com>
2021-06-01 14:27:55 +0500

    updated

--- rgba.js
+++
@@ -1,8 +0,0 @@
-// const rgba = ['rgba(r,g,b)']
-
-// const btn = document.querySelector('.btn');
-// const color = document.querySelector('.color');
-
-// btn.addEventListener('click', function(){
-    
-// })
